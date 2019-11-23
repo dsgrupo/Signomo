e97490157e10e44ea5426f29f3d90fd0f2ac320c
Felipe Bastos <felipepradobastos@gmail.com>
2019-11-22 22:51:08 -0300

    Tentando implementar webservice

---
+++ app/src/main/java/duj/app/signomo/Retrofit/RetrofitConfig.java
@@ -0,0 +1,22 @@
+package duj.app.signomo.Retrofit;
+
+import duj.app.signomo.Models.Usuario;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class RetrofitConfig {
+
+    private final Retrofit retrofit;
+
+    public RetrofitConfig() {
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl("https://login-service-signomo.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public UsuarioService getUsuarioService() {
+        return this.retrofit.create(UsuarioService.class);
+    }
+}
--- app/src/main/java/duj/app/signomo/SharedPreference/PreferenceUtils.java
+++ app/src/main/java/duj/app/signomo/SharedPreference/PreferenceUtils.java
@@ -22,6 +22,32 @@ public class PreferenceUtils {
         return prefs.getString(PreferenceKeys.KEY_NOME, null);
     }
 
+    public static boolean saveId(String id, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString(PreferenceKeys.KEY_NOME, id);
+        prefsEditor.apply();
+        return true;
+    }
+
+    public static String getId(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(PreferenceKeys.KEY_NOME, null);
+    }
+
+    public static boolean savebirthTime(String birthTime, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString(PreferenceKeys.KEY_NOME, birthTime);
+        prefsEditor.apply();
+        return true;
+    }
+
+    public static String getbirthTime(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(PreferenceKeys.KEY_NOME, null);
+    }
+
     public static boolean saveEmail(String email, Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor prefsEditor = prefs.edit();
